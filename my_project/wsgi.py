67307e46a3066987a8ee2600dee7bffdd812a052
Vishaldonda <vishal.d19@iiits.in>
2025-12-05 11:17:19 +0530

    Auto superuser setup

--- my_project/wsgi.py
+++ my_project/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for my_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_project.settings.production")
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_project.settings.dev")
+# Auto-create superuser on startup
+try:
+    from startup_superuser import create_superuser
+    create_superuser()
+except Exception as e:
+    print(f"Superuser creation skipped: {e}")
 
 application = get_wsgi_application()
